d5286d7c4b25d35b9c419c9bc216a9704b84d31c
hoseok <hosuk0614@gmail.com>
2024-12-01 14:58:52 +0900

    final3

--- example_folder/__init__.py
+++ example_folder/__init__.py
@@ -1,4 +1,4 @@
-from .tavily_search import search_on_web_1, search_on_web_2
+from .tavily_search import search_on_web_1, search_on_web_2, TavilySearch
 from .prompts import topic_prompt, objective_prompt, search_prompt, validate_prompt
 from .log_printer import log_printer
-__all__ = ["process_query", "search_on_web_1", "search_on_web_2", "topic_prompt", "objective_prompt", "validate_prompt", "log_printer", "search_prompt"]
+__all__ = ["TavilySearch", "search_on_web_1", "search_on_web_2", "topic_prompt", "objective_prompt", "validate_prompt", "log_printer", "search_prompt"]
